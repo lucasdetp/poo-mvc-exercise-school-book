31ed4d2dfef12bb8ca0d5a9cda80af9952b8a110
lucasdetp <detplucas@mail.com>
2023-01-17 21:31:45 +0100

    feat(exercice 3): Order students on their age

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Student;
 
 class StudentController extends Controller
 {
-    public function list()
+    public function list(Request $request)
     {
-        $students = Student::paginate(20);
+        $orderBy = $request->input('order_by', 'id');
+        $order = $request->input('order', 'asc');
+        $students = Student::orderBy($orderBy, $order)->paginate(20);
         return view('student_show', compact('students'));
     }
 }
--- resources/views/student_show.blade.php
+++ resources/views/student_show.blade.php
@@ -6,6 +6,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Students</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        .pagination {
+            display: flex;
+            justify-content: center;
+            text-align: center;
+        }
+
+        .pagination .page-item {
+            display: inline-block;
+            font-size: smaller;
+            padding: 0.5rem 0.75rem;
+        }
+
+        .w-5.h-5 {
+            width: 35px;
+            height: 35px;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -15,7 +50,8 @@
                 <th>#</th>
                 <th>Nom</th>
                 <th>Prénom</th>
-                <th>Détails</th>
+                <th>Téléphone</th>
+                <th>Date de naissance</th>
             </tr>
             @foreach ($students as $student)
             <tr>
@@ -23,6 +59,7 @@
                 <td>{{ $student->family_name }}</td>
                 <td>{{ $student->given_name }}</td>
                 <td>{{ $student->phone }}</td>
+                <td>{{ $student->birthday }}</td>
             </tr>
             @endforeach
         </table>
