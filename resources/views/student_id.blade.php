fa489058ebc00114897edfd70da447a3131c87bf
lucasdetp <detplucas@mail.com>
2023-01-18 12:38:46 +0100

    feat(exercice 5): Display the classes of a student

--- resources/views/student_id.blade.php
+++ resources/views/student_id.blade.php
@@ -4,14 +4,16 @@
 <head>
     <title>Informations</title>
     <style>
-        h1 {
+        h1,
+        h2 {
             font-size: 2em;
             font-weight: bold;
             text-align: center;
             margin-bottom: 40px;
         }
 
-        p {
+        p,
+        ul {
             font-size: 1.2em;
             margin-bottom: 10px;
             text-align: center;
@@ -21,6 +23,10 @@
             font-weight: bold;
             margin-right: 10px;
             text-align: center;
+        }
+
+        li {
+            list-style-type: none;
         }
     </style>
 </head>
@@ -32,6 +38,13 @@
     <p>Email: {{ $student->email }}</p>
     <p>Adresse: {{ $student->street_address }}</p>
     <p>Genre: {{$student->gender}}</p>
+    <h2>Classes</h2>
+    <ul>
+        @foreach ($student->school_classes as $class)
+        <li>{{ $class->year }} : {{ $class->class }} {{ $class->number }}</li>
+        @endforeach
+    </ul>
+
 </body>
 
 </html>
